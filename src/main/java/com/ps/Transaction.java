34f0bb75545c363e5e32b142f81cefe281fd993c
linotmike <128604219+linotmike@users.noreply.github.com>
2024-10-11 12:14:18 -0700

    added getters and setters to the Transactions class

--- src/main/java/com/ps/Transaction.java
+++ src/main/java/com/ps/Transaction.java
@@ -22,4 +22,44 @@ public class Transaction {
         this.vendor = vendor;
         this.amount = amount;
     }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
